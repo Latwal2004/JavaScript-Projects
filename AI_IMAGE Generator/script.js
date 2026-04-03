6234c8f6c9a2b1da9dc2536afa1a0fd2bfbf9049
Latwal2004 <harishlatwal688@gmail.com>
2026-04-03 21:31:09 +0530

    handeled the form submisiion and input to form

--- AI_IMAGE Generator/script.js
+++ AI_IMAGE Generator/script.js
@@ -1,6 +1,30 @@
 const themeToggle = document.querySelector(".theme-toggle");
-// const promptBtn = document.querySelector(".prompt-btn");
+const promptForm = document.querySelector(".prompt-form");
+const promptInput = document.querySelector(".prompt-input");
+const promptBtn = document.querySelector(".prompt-btn");
+const modelSelect = document.querySelector("#model-select");
+const countSelect = document.querySelector("#count-select");
+const ratioSelect = document.querySelector("#ratio-select");
 
+
+// Example prompts
+const examplePrompts = [
+  "A magic forest with glowing plants and fairy homes among giant mushrooms",
+  "An old steampunk airship floating through golden clouds at sunset",
+  "A future Mars colony with glass domes and gardens against red mountains",
+  "A dragon sleeping on gold coins in a crystal cave",
+  "An underwater kingdom with merpeople and glowing coral buildings",
+  "A floating island with waterfalls pouring into clouds below",
+  "A witch's cottage in fall with magic herbs in the garden",
+  "A robot painting in a sunny studio with art supplies around it",
+  "A magical library with floating glowing books and spiral staircases",
+  "A Japanese shrine during cherry blossom season with lanterns and misty mountains",
+  "A cosmic beach with glowing sand and an aurora in the night sky",
+  "A medieval marketplace with colorful tents and street performers",
+  "A cyberpunk city with neon signs and flying cars at night",
+  "A peaceful bamboo forest with a hidden ancient temple",
+  "A giant turtle carrying a village on its back in the ocean",
+];
 //set theme based on saved preferences or system defaults.
 (() =>{
 const savedTheme  = localStorage.getItem("theme");
@@ -18,4 +42,23 @@ const toggleTheme = () =>{
     localStorage.setItem("theme",isDarkTheme ? "dark" : "light");
     themeToggle.querySelector("i").className = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon";
 };
+
+//handle form submission
+const handleFormSubmit = (e) =>{
+ e.preventDefault();
+    //get form values 
+    const selectModal = modelSelect.value;
+    const imageCount = parseInt(countSelect.value) || 1;
+    const aspectRatio = ratioSelect.value || "1/1";
+    const promptText = promptInput.value.trim();
+
+    console.log(selectModal, imageCount, aspectRatio, promptText)
+}
+
+promptBtn.addEventListener("click", () =>{
+const prompt  = examplePrompts[Math.floor(Math.random() * examplePrompts.length)];
+promptInput.value =  prompt;
+promptInput.focus();
+});
+promptForm.addEventListener("submit", handleFormSubmit);
 themeToggle.addEventListener("click",toggleTheme);
