e3f178f9b4c557201abf66471131673712dd8982
Latwal2004 <harishlatwal688@gmail.com>
2026-04-04 14:01:21 +0530

    added the sidebar collapsed class and set ui acc to toggle on it

---
+++ sidebar_Menu/script.js
@@ -0,0 +1,5 @@
+const sidebar = document.querySelector(".sidebar");
+const sidebarToggleBtn = document.querySelector(".sidebar-toggle");
+sidebarToggleBtn.addEventListener("click",() =>{
+sidebar.classList.toggle("collapsed");
+});
