3fdac605bb1285973e7488d960ae7cc6cef254df
Latwal2004 <harishlatwal688@gmail.com>
2026-04-16 20:06:59 +0530

    some fixes to api if fasils

--- Image_Search_page/script.js
+++ Image_Search_page/script.js
@@ -9,40 +9,51 @@ let inputData = "";
 let page = 1;
 
 async function searchImages(){
-inputData = inputElement.value;
-const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
-const response  = await fetch(url);
-const data = await response.json();
+inputData = inputElement.value.trim();
+if(!inputData)
+    {
+        alert("Please Enter something for results.....!");
+        return;
+    }
+ try{
+        showMoreBtn.textContent = 'Loading...'
+
+        const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+        const response  = await fetch(url);
+        const data = await response.json();
 
 
-const results = data.results;
-if(page === 1){
-    searchResults.innerHTML = "";
+        const results = data.results;
+        if(page === 1){
+            searchResults.innerHTML = "";
+        }
+
+        results.forEach((result) =>{
+        const imageWrapper = document.createElement('div');
+        imageWrapper.classList.add("search-result")
+
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        image.alt = result.alt_description || "Image";
+
+        const imageLink = document.createElement('a');
+        imageLink.href = result.links.html
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description || "View Image";
+
+        imageWrapper.append(image,imageLink)
+        searchResults.append(imageWrapper);
+    });
+
+        page++;
+        showMoreBtn.style.display = 'block';
+        showMoreBtn.textContent = 'Show More';
+    }
+    catch (error) {
+        console.error(error);
+        alert("Something went wrong!");
+    }
 }
-
-results.map((result) =>{
-    const imageWrapper = document.createElement('div');
-    imageWrapper.classList.add("search-result")
-
-    const image = document.createElement('img');
-    image.src = result.urls.small;
-    image.alt = result.alt_description;
-
-    const imageLink = document.createElement('a');
-    imageLink.href = result.links.html
-    imageLink.target = "_blank";
-    imageLink.textContent = result.alt_description;
-
-    imageWrapper.append(image,imageLink)
-    searchResults.append(imageWrapper);
-});
-
-page++;
-if(page > 1){
-    showMoreBtn.style.display = 'block';
-}
-}
-
 formElement.addEventListener("submit",(event) =>{
     event.preventDefault();
     page = 1;
